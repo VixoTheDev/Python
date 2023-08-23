5bc7b24b0612a74eeda171412cddeaa9081a90a0
VixoTheDev <140670970+VixoTheDev@users.noreply.github.com>
2023-08-23 20:17:46 +0200

    using functions in python

---
+++ functions.py
@@ -0,0 +1,7 @@
+# Define a function that adds two numbers
+def add_numbers(a, b):
+    return a + b
+
+# Call the function and print the result
+result = add_numbers(5, 3)
+print("The sum is:", result)
