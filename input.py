2e5a2ba3ff7d50f9523e3f00d6ff8ef3e506ebdb
VixoTheDev <140670970+VixoTheDev@users.noreply.github.com>
2023-08-23 20:20:45 +0200

    Using input and include in python

---
+++ input.py
@@ -0,0 +1,8 @@
+myinput = input("something: ")
+
+if myinput == "hi":
+  print("Hi, how are you?")
+elif myinput.include("2137"):
+  print("Wow You included JP2 Joke")
+else:
+  print("nothing here")
