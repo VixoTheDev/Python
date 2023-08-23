804fd95164b88518645b8a9e81b248e55f8f711f
VixoTheDev <140670970+VixoTheDev@users.noreply.github.com>
2023-08-23 20:22:28 +0200

    Working with modules in python

---
+++ modules.py
@@ -0,0 +1,11 @@
+import math
+
+radius = 5
+area = math.pi * radius ** 2
+print("Area of the circle:", area)
+
+# Alternatively, you can import specific functions/classes
+from random import randint
+
+random_num = randint(1, 10)
+print("Random number between 1 and 10:", random_num)
