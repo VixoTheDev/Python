fe08de32f8eb287c263d81acd2c46dd7e484b098
VixoTheDev <140670970+VixoTheDev@users.noreply.github.com>
2023-08-23 20:24:43 +0200

    Working with Database in python using sqlite3

---
+++ database.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+conn = sqlite3.connect("example.db")
+cursor = conn.cursor()
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS users
+                (id INTEGER PRIMARY KEY, name TEXT, email TEXT)''')
+
+cursor.execute("INSERT INTO users (name, email) VALUES (?, ?)", ("Alice", "alice@example.com"))
+conn.commit()
+
+cursor.execute("SELECT * FROM users")
+users = cursor.fetchall()
+for user in users:
+    print("User:", user)
+
+conn.close()
